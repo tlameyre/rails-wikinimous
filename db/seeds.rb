728c1bae9fc10a1b7aae4f2a72c9f409b7eb4500
Theotime Lameyre <theotime.lameyre@gmail.com>
2020-08-11 21:53:31 +0200

    Fin du css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Creating 10 fake articles...'
+Article.destroy_all
+10.times do
+  article = Article.new(
+    title: Faker::Movie.title,
+    content: Faker::Lorem.paragraph(sentence_count: 30)
+  )
+  article.save!
+end
+puts 'Finished!'
